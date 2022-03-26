f3b8c40637f9c8005861976563c583c2a359c2f0
minsu-kim <minsu8522@gmail.com>
2022-03-26 14:35:07 +0900

    added methods menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -7,68 +7,83 @@ public class MenuManager {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num = 5;
-		
+
 		while(num != 6) {
-			
 			System.out.println("1. Add Money"); 
-			//println을 통해 Add Money 출력 후 줄 바꿈.
-
 			System.out.println("2. Delete Money");
-			//println을 통해 Delete Money 출력 후 줄 바꿈.
-
 			System.out.println("3. Edit Money");
-			//println을 통해 Edit Money 출력 후 줄 바꿈.
-
 			System.out.println("4. View Money");
-			//println을 통해 View Money 출력 후 줄 바꿈.
-			
 			System.out.println("5. Show a Menu");
-			//println을 통해 Show a Menu 출력 후 줄 바꿈.
-
 			System.out.println("6. Exit");
-			//println을 통해 Exit 출력 후 줄 바꿈.
-			
 			System.out.println("Select one number beetween 1~6 : ");
-			
 			num = input.nextInt();
-			
+
+			//각 메뉴에 대한 출력
 			if (num == 1)
 			{
 				System.out.print(" Money Input : ");
 				int money = input.nextInt();
 				System.out.print(" Input Day : ");
 				String day = input.next(); //nextLine 사용시 오류 발생
-				
+
 			}
 			else if (num == 2)
 			{
-				System.out.print(" Using Money : ");
-				int money = input.nextInt();
-				System.out.print(" Left Money : ");
-				String day = input.next();
+				deleteMoney();
 			}
 			else if (num == 3)
 			{
-				System.out.print(" Money Input : ");
-				int money = input.nextInt();
+				editMoney();
 			}
 			else if (num == 4)
 			{
-				System.out.print(" Money Input : ");
-				int money = input.nextInt();
+				viewMoney();
 			}
 			else {
 				continue;
 				//5번을 입력 시 메뉴를 다시 출력해줌
 			}
-			
-			
-			
-			
-			
-		} //while 문 종료 괄호
-		
 
+
+
+
+
+		} //while 문 종료 괄호
+
+
+	}
+	
+	//각 메뉴에 대한 메소드
+	//입력값을 저장하는 메소드
+	
+	public static void addMoney() {
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Money Input : ");
+		int inputMoney = input.nextInt();
+		System.out.print(" Input Day : ");
+		String day = input.next();
+	}
+	
+	public static void deleteMoney() {
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Money Input : ");
+		int inputMoney = input.nextInt();
+		System.out.print(" Using Money : ");
+		int usedMoney = input.nextInt();
+		System.out.print(" Left Money : ");
+		String day = input.next();
+	}
+	
+	public static void editMoney() {
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Money Input : ");
+		int inputMoney = input.nextInt();
+	}
+	
+	public static void viewMoney() {
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Money Input : ");
+		int inputMoney = input.nextInt();
 	}
 
 }
