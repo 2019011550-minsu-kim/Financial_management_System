180bda2b97a41b6a47a2c13717d7ef12986af7c8
minsu-kim <minsu8522@gmail.com>
2022-04-02 17:14:54 +0900

    added some class

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,76 +3,48 @@ import java.util.Scanner;
 
 public class MenuManager {
 	//원하는 메뉴를 출력하는 클래스
+	
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int num = 5;
+		FinancialManager financialManager = new FinancialManager(input);
+		
+		
+		
+		int num = -1;
 
-		while(num != 6) {
+		while(num != 5) {
 			System.out.println("1. Add Money"); 
 			System.out.println("2. Delete Money");
 			System.out.println("3. Edit Money");
 			System.out.println("4. View Money");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number beetween 1~6 : ");
+			System.out.println("5. Exit");
+			System.out.println("Select one number beetween 1~5 : ");
 			num = input.nextInt();
 
 			//각 메뉴에 대한 출력
 			if (num == 1)
 			{
-				addMoney();
+				financialManager.addMoney();
 			}
 			else if (num == 2)
 			{
-				deleteMoney();
+				financialManager.deleteMoney();
 			}
 			else if (num == 3)
 			{
-				editMoney();
+				financialManager.editMoney();
 			}
 			else if (num == 4)
 			{
-				viewMoney();
+				financialManager.viewMoney();
 			}
 			else {
 				continue;
 				//5번을 입력 시 메뉴를 다시 출력해줌
 			}
 		} //while 문 종료 괄호
-	}
+	}//main method 괄호
 	
-	//각 메뉴에 대한 메소드
-	//입력값을 저장하는 메소드
-	
-	public static void addMoney() {
-		Scanner input = new Scanner(System.in);
-		System.out.print(" Money Input : ");
-		int inputMoney = input.nextInt();
-		System.out.print(" Input Day : ");
-		String day = input.next();
-	}
-	
-	public static void deleteMoney() {
-		Scanner input = new Scanner(System.in);
-		System.out.print(" Money Input : ");
-		int inputMoney = input.nextInt();
-		System.out.print(" Using Money : ");
-		int usedMoney = input.nextInt();
-		System.out.print(" Left Money : ");
-		String day = input.next();
-	}
-	
-	public static void editMoney() {
-		Scanner input = new Scanner(System.in);
-		System.out.print(" Money Input : ");
-		int inputMoney = input.nextInt();
-	}
-	
-	public static void viewMoney() {
-		Scanner input = new Scanner(System.in);
-		System.out.print(" Money Input : ");
-		int inputMoney = input.nextInt();
-	}
 
 }
